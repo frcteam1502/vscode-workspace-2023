ac5b71589b71ef1c035f4304ae41bd130988715e
nickSpruce <emeraldspruce2005@gmail.com>
2023-02-25 11:38:52 -0500

    woah

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,6 +1,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.commands.MotorTestCommand;
 import frc.robot.subsystems.MotorTestSubsystem;
 
 public class RobotContainer {
@@ -12,7 +13,7 @@ public class RobotContainer {
   }
 
   private void configureBindings() {
-    motorTestSubsystem.setDefaultCommand(null);
+    motorTestSubsystem.setDefaultCommand(new MotorTestCommand(motorTestSubsystem));
 
     Constants.XboxButtons.LEFT_BUMPER.onTrue(new InstantCommand(motorTestSubsystem::BACKWARD)).onFalse(new InstantCommand(motorTestSubsystem::STOP));
     Constants.XboxButtons.RIGHT_BUMPER.onTrue(new InstantCommand(motorTestSubsystem::FORWARD)).onFalse(new InstantCommand(motorTestSubsystem::STOP));
