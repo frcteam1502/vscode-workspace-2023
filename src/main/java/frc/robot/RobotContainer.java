99c852e78f6fe3c3d76c879efaafd97f48e738b9
nickSpruce <emeraldspruce2005@gmail.com>
2023-02-26 17:34:41 -0500

    Working intake

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,6 +1,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.IntakeSubsystem;
 
 public class RobotContainer {
@@ -13,7 +14,7 @@ public class RobotContainer {
   }
 
   private void configureBindings() {
-    Constants.XboxButtons.LEFT_BUMPER.onTrue(new InstantCommand(IntakeSubsystem::OnPressed));
-    Constants.XboxButtons.LEFT_BUMPER.onFalse(new InstantCommand(IntakeSubsystem::OnReleased));
+    Constants.XboxButtons.LEFT_BUMPER.onTrue(new InstantCommand(IntakeSubsystem::OnPressed).andThen(new WaitCommand(.25)));
+    Constants.XboxButtons.LEFT_BUMPER.onFalse(new InstantCommand(IntakeSubsystem::OnReleased).andThen(new WaitCommand(.25)));
   }
 }
