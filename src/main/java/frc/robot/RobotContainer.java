bcef178f88c48c5160143d75019a7d44e143a0b9
jonathan berent (mentor) <jonathan.berent@vsolvit.com>
2023-02-27 21:11:59 -0500

    quick gripper test

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,18 +11,19 @@ import frc.robot.Constants.ArmConstants;
 import frc.robot.Constants.XboxButtons;
 import frc.robot.commands.ArmByController;
 import frc.robot.commands.DriveByController;
+import frc.robot.commands.GripperByController;
 import frc.robot.commands.Autonomous.Selection.SimpleBalance;
 import frc.robot.commands.Autonomous.Simple.AutoBalance;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.DriveTrain;
-// import frc.robot.subsystems.GripperSubsystem;
+import frc.robot.subsystems.GripperSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 
 public class RobotContainer {
   //Subsystems
   private final ArmSubsystem armSubsystem = new ArmSubsystem(ArmConstants.LEAD_DEVICE_ID, ArmConstants.FOLOW_DEVICE_ID, ArmConstants.EXTEND_DEVICE_ID);
   private final IntakeSubsystem intakeSubsystem = new IntakeSubsystem();
-  // private final GripperSubsystem gripperSubsystem = new GripperSubsystem();
+  private final GripperSubsystem gripperSubsystem = new GripperSubsystem();
   private final DriveTrain driveSubsystem = new DriveTrain();
 
   //Autonomous Commands
@@ -53,10 +54,11 @@ public class RobotContainer {
     // XboxButtons.BUTTON_B.onTrue(new InstantCommand(armSubsystem::GoToMiddle));
     // XboxButtons.BUTTON_A.onTrue(new InstantCommand(armSubsystem::GoToFloor));
     // XboxButtons.BUTTON_X.onTrue(new InstantCommand(armSubsystem::GoToStow));
-
-    // //Gripper
-    // XboxButtons.RIGHT_BUMPER.onTrue(new InstantCommand(gripperSubsystem::toggleGripper));
-    // XboxButtons.LEFT_BUMPER.onTrue(new InstantCommand(gripperSubsystem::toggleRotate));
+    
+    //Gripper
+    gripperSubsystem.setDefaultCommand(new GripperByController(gripperSubsystem));
+    XboxButtons.RIGHT_BUMPER.toggleOnTrue(new InstantCommand(gripperSubsystem::toggleGripper));
+    XboxButtons.LEFT_BUMPER.toggleOnTrue(new InstantCommand(gripperSubsystem::toggleRotate));
   }
 
   private void setUpSendableChooser() {
--- src/main/java/frc/robot/subsystems/GripperSubsystem.java
+++ src/main/java/frc/robot/subsystems/GripperSubsystem.java
@@ -1,60 +1,75 @@
-// package frc.robot.subsystems;
+package frc.robot.subsystems;
 
 
-// import com.revrobotics.CANSparkMax;
-// import com.revrobotics.RelativeEncoder;
-// import com.revrobotics.SparkMaxPIDController;
-// import com.revrobotics.CANSparkMax.ControlType;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMax.ControlType;
 
-// import edu.wpi.first.wpilibj2.command.SubsystemBase;
-// import frc.robot.Constants;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
-// public class GripperSubsystem extends SubsystemBase {
-//   private final CANSparkMax gripper;
-//   private final CANSparkMax rotate;
+public class GripperSubsystem extends SubsystemBase {
 
-//   private final RelativeEncoder gripperEncoder;
-//   private final RelativeEncoder rotateEncoder;
+    private static final double MAX_GRIPPER_OPEN = 1.83333;
 
-//   private final SparkMaxPIDController gripperPID;
-//   private final SparkMaxPIDController rotatePID;
+  private final CANSparkMax gripper;
+  private final CANSparkMax rotate;
 
-//   private boolean turn = true;
-//   private double goalRotate = 0;
-//   private boolean open = true;
-//   private double goalOpen = 0;
+  private final RelativeEncoder gripperEncoder;
+  private final RelativeEncoder rotateEncoder;
 
-//   public GripperSubsystem() {
-//     //Motors
-//     gripper = Constants.Motors.GRIPPER;
-//     rotate = Constants.Motors.GRIPPER_ROTATE;
+  private final SparkMaxPIDController gripperPID;
+  private final SparkMaxPIDController rotatePID;
 
-//     //Encoders
-//     rotateEncoder = rotate.getEncoder();
-//     gripperEncoder = gripper.getEncoder();
+  private boolean turn = true;
+  private double goalRotate = 0;
+  private boolean open = true;
+  private double goalOpen = 0;
 
-//     rotateEncoder.setPosition(0);
-//     gripperEncoder.setPosition(0);
+  public GripperSubsystem() {
+    //Motors
+    gripper = Constants.Motors.GRIPPER;
+    rotate = Constants.Motors.GRIPPER_ROTATE;
+
+    //Encoders
+    rotateEncoder = rotate.getEncoder();
+    gripperEncoder = gripper.getEncoder();
+
+    rotateEncoder.setPosition(0);
+    gripperEncoder.setPosition(0);
     
-//     //PIDs
-//     gripperPID = gripper.getPIDController();
-//     rotatePID = rotate.getPIDController();
+    //PIDs
+    gripperPID = gripper.getPIDController();
+    gripperPID.setP(0.1);
+    rotatePID = rotate.getPIDController();
 
-//     gripperPID.setFeedbackDevice(gripperEncoder);
-//     rotatePID.setFeedbackDevice(rotateEncoder);
-//   }
+    gripperPID.setFeedbackDevice(gripperEncoder);
+    rotatePID.setFeedbackDevice(rotateEncoder);
+  }
 
-//   public void toggleGripper() {
-//     if(open) goalOpen = 0;
-//     else goalOpen = 10; //ARBITRATY VAUE
-//     turn = !turn;
-//     rotatePID.setReference(goalOpen, ControlType.kPosition);
-//   }
+  public void toggleGripper() {
+    if(open) goalOpen = 0;
+    else goalOpen = MAX_GRIPPER_OPEN; //Fully Open
+    open = !open;
+    gripperPID.setReference(goalOpen, ControlType.kPosition);
+  }
 
-//   public void toggleRotate() {
-//     if(turn) goalRotate = 0;
-//     else goalRotate = 10; //ARBITRATY VAUE
-//     turn = !turn;
-//     rotatePID.setReference(goalRotate, ControlType.kPosition);
-//   }
-// }
+  public void toggleRotate() {
+    if(turn) goalRotate = 0;
+    else goalRotate = 120; //ARBITRATY VAUE
+    turn = !turn;
+    rotatePID.setReference(goalRotate, ControlType.kPosition);
+  }
+
+  public void DisplayInfo()
+  {
+    SmartDashboard.putNumber("GRIPPER Position", gripperEncoder.getPosition());
+    SmartDashboard.putNumber("GRIPPER PID", gripperPID.getP());
+    SmartDashboard.putBoolean("GRIPPER Open", open);
+    SmartDashboard.putNumber("WRIST Position", rotateEncoder.getPosition());
+    SmartDashboard.putBoolean("Wrist Turn", turn);
+  }
+
+}
