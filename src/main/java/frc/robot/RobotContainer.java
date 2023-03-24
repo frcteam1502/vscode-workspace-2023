0e31a01a4f20c7a4a10e95bddfa75d75c1970069
jonathan berent (mentor) <jonathan.berent@vsolvit.com>
2023-03-23 22:20:09 -0400

    add Time Of Flight sensor

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,15 +3,19 @@ package frc.robot;
 import frc.robot.commands.DriveByController;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.PdpSubsystem;
+import frc.robot.subsystems.TimeOfFlightSubsystem;
 
 public class RobotContainer {
   //Subsystems
   private final DriveTrain driveSubsystem;
   private final PdpSubsystem pdpSubsystem;
+  private final TimeOfFlightSubsystem tofSubsystem;
 
   public RobotContainer() {
     driveSubsystem = new DriveTrain();
     pdpSubsystem = new PdpSubsystem();
+    tofSubsystem = new TimeOfFlightSubsystem();
+    
     configureBindings();
   }
 
